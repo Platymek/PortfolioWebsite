59a7dd75291c1da460506b5c73bf7df9098139ce
Platymek <hectordavid.selby@gmail.com>
2024-08-20 13:00:59 +0100

    Getting ready to make cards!
    - cards are where the games/ art/ software will be contained
    - got ready to make a grid of cards and cards themselves
    
    next:
    - make a card!

---
+++ components/card-grid/card-grid.tsx
@@ -0,0 +1,17 @@
+
+import Card from "./card/card";
+import styles from './card-grid.module.css'
+
+export default function CardGrid() {
+
+    return (
+
+        <div className={styles.cardGrid}>
+
+            <Card/>
+            <Card/>
+            <Card/>
+        
+        </div> 
+    )
+}
---
+++ components/card-grid/card/card.tsx
@@ -0,0 +1,10 @@
+
+import styles from '../card-grid.module.css'
+
+export default function Card() {
+
+    return (
+
+        <div className={styles.card}>I am a card</div>
+    )
+}
