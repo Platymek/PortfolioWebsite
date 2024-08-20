091c3fb629190027277667f08ba0faa9a88dd64c
Platymek <hectordavid.selby@gmail.com>
2024-08-20 18:47:22 +0100

    card almost finished
    - card almost finished, just needs to take props
    - fixed abhorrent css stuff with filling in the card on mouse hover
    
    next:
    - add props to card

--- components/card-grid/card-grid.tsx
+++ components/card-grid/card-grid.tsx
@@ -11,6 +11,9 @@ export default function CardGrid() {
             <Card/>
             <Card/>
             <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         
         </div> 
     )
