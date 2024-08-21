0b44bd874162aca5b5317b547df5b0a37275d3ca
Platymek <hectordavid.selby@gmail.com>
2024-08-21 11:18:44 +0100

    put everything in the app folder
    - moved everything to the app folder to allow dynamic loading or something?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import NavBar from "../components/navbar/navbar";
+import NavBar from "./components/navbar/navbar";
 import { ThemeProvider } from "./theme-provider";
 
 import "./globals.css";
