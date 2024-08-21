875babc5118a278c8d0a8ce7874e89b7ae5e7b82
Platymek <hectordavid.selby@gmail.com>
2024-08-21 10:40:45 +0100

    Update for pages
    - updated the site to allow it to be run on pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,33 @@ import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 }
  
 const withMDX = createMDX({
